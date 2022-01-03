d09c774aa1788d89f10717795aef916a5eb86d74
quangkhai <vukhaiabc@gmail.com>
2022-01-03 21:54:49 +0700

    update docker

--- djangofabbi/settings.py
+++ djangofabbi/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'fabbi_data',
         'USER': 'root',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
 }
